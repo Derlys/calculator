36ac15815a2dacfd15123bc1a25540da23795165
Derlys Dominguez <derlys275@gmail.com>
2023-06-22 16:02:03 +0100

    add:calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calculator';
+  num1?: number;
+  num2?: number;
+  result?: number;
+
+  sum() {
+    if (this.num1 && this.num2) {
+      this.result = this.num1 + this.num2;
+    } else {
+      alert('Can not sum');
+    }
+  }
+  subtract() {
+    if (this.num1 && this.num2) {
+      this.result = this.num1 - this.num2;
+    } else {
+      alert('Can not subtract');
+    }
+  }
 }
